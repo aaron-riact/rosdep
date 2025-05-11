f57a396d26a6e86e9881fccae70f5f58f7482c7e
Aaron Lipinski <kris.lipinski@gmail.com>
2025-05-11 22:17:38 +0200

    add a --one-shot option

--- src/rosdep2/main.py
+++ src/rosdep2/main.py
@@ -355,6 +355,13 @@ def _rosdep_main(args):
                            'is in the provided list. The option can be supplied '
                            'multiple times. A space separated list of installers can also '
                            'be passed as a string. Example: `--filter-for-installers "apt pip"`')
+    parser.add_option('--one-shot', action='append', default=[],
+                      metavar='INSTALLER_KEY',
+                      help="Affects the 'install' verb. If supplied, each installer "
+                           'that supports it, will use a single invocation to install '
+                           'its packages. The option can be supplied '
+                           'multiple times. A space separated list of installers can also '
+                           'be passed as a string. Example: `--one-shot "apt pip"`')
     parser.add_option('--from-paths', dest='from_paths',
                       default=False, action='store_true',
                       help="Affects the 'check', 'keys', and 'install' verbs. "
